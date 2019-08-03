93fcfa5040eedd66db650bfcf9faab10a7eae259
Mr <mickaelmendes50@gmail.com>
2019-08-03 16:35:55 -0300

    activities: Use ViewHolder
    Signed-off-by: Mr <mickaelmendes50@gmail.com>

---
+++ java/com/mesquita/festafimdeano/view/DetailsActivity.java
@@ -0,0 +1,24 @@
+package com.mesquita.festafimdeano.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.CheckBox;
+
+import com.mesquita.festafimdeano.R;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    private ViewHolder mViewHolder = new ViewHolder();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+
+        this.mViewHolder.checkParticipate = findViewById(R.id.check_participate);
+    }
+
+    private static class ViewHolder {
+        CheckBox checkParticipate;
+    }
+}
---
+++ java/com/mesquita/festafimdeano/view/MainActivity.java
@@ -0,0 +1,41 @@
+package com.mesquita.festafimdeano.view;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.mesquita.festafimdeano.R;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ViewHolder mViewHolder = new ViewHolder();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.mViewHolder.textToday = findViewById(R.id.text_today);
+        this.mViewHolder.textDaysLeft = findViewById(R.id.text_days_left);
+        this.mViewHolder.buttonConfirm = findViewById(R.id.button_confirm);
+
+        this.mViewHolder.buttonConfirm.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick (View v) {
+        if (v.getId() == R.id.button_confirm) {
+            Intent intent = new Intent(this, DetailsActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    private static class ViewHolder {
+        TextView textToday;
+        TextView textDaysLeft;
+        Button buttonConfirm;
+    }
+}
