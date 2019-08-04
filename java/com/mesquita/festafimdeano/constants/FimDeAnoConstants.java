cad850f0ac2671e48bb58480f5d91a94bf5eedac
Mr <mickaelmendes50@gmail.com>
2019-08-04 16:22:18 -0300

    details: Save checkbox status

---
+++ java/com/mesquita/festafimdeano/constants/FimDeAnoConstants.java
@@ -0,0 +1,9 @@
+package com.mesquita.festafimdeano.constants;
+
+public class FimDeAnoConstants {
+
+    public static String PRESENCE_KEY = "presenceKey";
+    public static String CONFIRMATION_YES = "CONFIRMATION_YES";
+    public static String CONFIRMATION_NO = "CONFIRMATION_NO";
+
+}
--- java/com/mesquita/festafimdeano/view/DetailsActivity.java
+++ java/com/mesquita/festafimdeano/view/DetailsActivity.java
@@ -2,20 +2,40 @@ package com.mesquita.festafimdeano.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.CheckBox;
 
 import com.mesquita.festafimdeano.R;
+import com.mesquita.festafimdeano.constants.FimDeAnoConstants;
+import com.mesquita.festafimdeano.data.SecurityPreferences;
 
-public class DetailsActivity extends AppCompatActivity {
+public class DetailsActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ViewHolder mViewHolder = new ViewHolder();
+    private SecurityPreferences mSecurityPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
 
+        this.mSecurityPreferences = new SecurityPreferences(this);
+
         this.mViewHolder.checkParticipate = findViewById(R.id.check_participate);
+        this.mViewHolder.checkParticipate.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.check_participate) {
+            if (this.mViewHolder.checkParticipate.isChecked()) {
+                // Salvar presença
+                this.mSecurityPreferences.storeString(FimDeAnoConstants.PRESENCE_KEY, FimDeAnoConstants.CONFIRMATION_YES);
+            } else {
+                // Salvar ausencia
+                this.mSecurityPreferences.storeString(FimDeAnoConstants.PRESENCE_KEY, FimDeAnoConstants.CONFIRMATION_NO);
+            }
+        }
     }
 
     private static class ViewHolder {
